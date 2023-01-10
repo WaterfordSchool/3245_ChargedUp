f4aea052f58c5b4b7616ef2c5c6d5a7f093134d2
laurenw2 <cheeseandcrackers333@gmail.com>
2023-01-10 10:29:07 -0700

    auto cleanup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.AutoCommandBalance;
 
 
 public class Robot extends TimedRobot {
